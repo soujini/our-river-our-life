a0a4a5b6e05179f2ee92456318adfae1a0083184
souj <soujanya.venkatesh@gmail.com>
2020-08-12 16:28:24 +0530

    Maps

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -6,7 +6,6 @@ import { timer } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 declare var google;
 
-
 @Component({
   selector: 'app-maps',
   templateUrl: './maps.component.html',
@@ -14,26 +13,21 @@ declare var google;
 })
 export class MapsComponent implements OnInit {
   images = [];
+  // myForm = new FormGroup({
+  //   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  //   file: new FormControl('', [Validators.required]),
+  //   fileSource: new FormControl('', [Validators.required])
+  // });
 
-  myForm = new FormGroup({
-
-   name: new FormControl('', [Validators.required, Validators.minLength(3)]),
-
-   file: new FormControl('', [Validators.required]),
-
-   fileSource: new FormControl('', [Validators.required])
-
- });
   @ViewChild('search', { static: true }) public searchElementRef: ElementRef;
-  date = new FormControl(new Date());
+  // date = new FormControl(new Date());
   // public time = new Date();
   activeColor: string = 'green';
   mapsForm: FormGroup;
-  imagesArray:any;
+  // imagesArray:any;
   geocoder:any;
   baseColor: string = '#ccc';
   overlayColor: string = 'rgba(255,255,255,0.5)';
-
   dragging: boolean = false;
   loaded: boolean = false;
   imageLoaded: boolean = false;
@@ -70,54 +64,20 @@ export class MapsComponent implements OnInit {
     private ngZone: NgZone
   ) {
     this.createForm();
-    this.imagesArray = this.mapsForm.controls.images as FormArray;
+    // this.imagesArray = this.mapsForm.controls.images as FormArray;
 
-   }
- 
+  }
+
   mapReady(event) {
     // console.log(event);
     this.setCurrentPosition();
   }
-   codeLatLng(lat, lng) {
-     var itemLocality;
-     this.geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(lat, lng);
-    this.geocoder.geocode({latLng: latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[1]) {
-          var arrAddress = results;
-          console.log(results);
-          for ( var i=0;i<results;i++)
-          {
-           if (i==0){
-            if (results[i].types[0] == "locality") {
-              console.log("City: " + results[i].address_components[0].long_name);
-              itemLocality = results[i].address_components[0].long_name;
-            }
-          }
-          }
-     
-        } else {
-          alert("No results found");
-        }
-      } else {
-        alert("Geocoder failed due to: " + status);
-      }
-    });
-  }
+
 
   ngOnInit() {
     //set google maps defaults
     this.zoom = 15;
-    /* this.latitude = 39.8282;
-    this.longitude = -98.5795; */
-
-    //create search FormControl
     this.searchControl = new FormControl();
-
-    //set current position
-
-    // this.recenterMap()
 
     //load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
@@ -126,9 +86,7 @@ export class MapsComponent implements OnInit {
       });
       autocomplete.addListener("place_changed", () => {
         this.ngZone.run(() => {
-          //get the place result
           let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
           //verify result
           if (place.geometry === undefined || place.geometry === null) {
             return;
@@ -137,83 +95,88 @@ export class MapsComponent implements OnInit {
           //set latitude, longitude and zoom
           this.latitude = place.geometry.location.lat();
           this.longitude = place.geometry.location.lng();
-          this.zoom = 12;
+          // this.zoom = 12;
         });
       });
     });
   }
-  recenterMap() {
-    this.latitude = 36.8392542;
-    this.longitude = 10.313922699999999;
-  }
+
+  // recenterMap() {
+  //   this.latitude = 36.8392542;
+  //   this.longitude = 10.313922699999999;
+  // }
 
   createForm() {
     this.mapsForm = this.fb.group({
       location:[''],
       latitude:[''],
       longitude:[''],
-      date:[''],
-      time:[''],
-      photos:this.fb.array([]),
+      activityDate:[(new Date())],
+      activityTime:[''],
+      photos: this.fb.array([]),
       experience:['']
     });
-
-
   }
+
+  async getAddressByLatitudeAndLongitude(lat, lng, form) {
+    var address;
+    this.geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(lat, lng);
+
+    await this.geocoder.geocode({latLng: latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        var arrAddress = results;
+        address = results[0].formatted_address;
+        form.patchValue({
+          location: address
+        });
+      } else {
+        console.log("Geocoder failed due to: " + status);
+      }
+    });
+  }
+
   private setCurrentPosition() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-        this.mapsForm.patchValue ({
+        this.mapsForm.patchValue({
           latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          location: 'kaveri',
-          experience:'',
-          date:'',
-          time:''
-        }) ;
-        this.codeLatLng(12.385129, 75.491320);
-        // this.codeLatLng( position.coords.latitude, position.coords.longitude);
-        // this.longitude = position.coords.longitude;
-        this.zoom = 15;
-        //console.log(this.latitude, this.longitude);
+          longitude: position.coords.latitude,
+        });
+        this.getAddressByLatitudeAndLongitude(position.coords.latitude + (0.0000000000100 * Math.random()), position.coords.latitude + (0.0000000000100 * Math.random()), this.mapsForm);
+        // this.zoom = 15;
       });
     }
-  }
-
-  getPosition() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.latitude = position.coords.latitude + (0.0000000000100 * Math.random());
-        this.longitude = position.coords.longitude + (0.0000000000100 * Math.random());
-      });
-    } else {
+    else {
       alert("Geolocation is not supported by this browser.");
     }
   }
-  addAlert() {
-    console.log(this.mapsForm.value);
 
+  // getPosition() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position) => {
+  //       this.latitude = position.coords.latitude + (0.0000000000100 * Math.random());
+  //       this.longitude = position.coords.longitude + (0.0000000000100 * Math.random());
+  //     });
+  //   } else {
+  //     alert("Geolocation is not supported by this browser.");
+  //   }
+  // }
+  addAlert() {
+
+    console.log(this.mapsForm.value);
     // this.addAlert=true;
   }
-
-  // handleDragEnter() {
-  //   this.dragging = true;
-  // }
-
-  // handleDragLeave() {
-  //   this.dragging = false;
-  // }
 
   // handleDrop(e) {
   //   e.preventDefault();
   //   this.dragging = false;
   //   this.handleInputChange(e);
   // }
-
-  handleImageLoad() {
-    this.imageLoaded = true;
-  }
+  //
+  // handleImageLoad() {
+  //   this.imageLoaded = true;
+  // }
 
   // handleInputChange(e) {
   //   var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
@@ -232,66 +195,31 @@ export class MapsComponent implements OnInit {
   //   reader.readAsDataURL(file);
   // }
 
-  _handleReaderLoaded(e) {
-    var reader = e.target;
-    this.imageSrc = reader.result;
-    this.loaded = true;
-  }
-  get f(){
-
-    return this.myForm.controls;
-
-  }
+  // _handleReaderLoaded(e) {
+  //   var reader = e.target;
+  //   this.imageSrc = reader.result;
+  //   this.loaded = true;
+  // }
+  // get f(){
+  //
+  //   return this.myForm.controls;
+  //
+  // }
   onFileChange(event) {
-
     if (event.target.files && event.target.files[0]) {
-
-        var filesAmount = event.target.files.length;
-
-        for (let i = 0; i < filesAmount; i++) {
-
-                var reader = new FileReader();
-
-   
-
-                reader.onload = (event:any) => {
-
-                  console.log(event.target.result);
-
-                   this.images.push(event.target.result); 
-
-   
-
-                   this.myForm.patchValue({
-
-                      fileSource: this.images
-
-                   });
-
-                }
-
-  
-
-                reader.readAsDataURL(event.target.files[i]);
-
+      var filesAmount = event.target.files.length;
+      for (let i = 0; i < filesAmount; i++) {
+        var reader = new FileReader();
+        reader.onload = (event:any) => {
+          console.log(event.target.result);
+          this.images.push(event.target.result);
+          // this.mapsForm.patchValue({
+          //   photos:this.images
+          //   // fileSource: this.images
+          // });
         }
-
+        reader.readAsDataURL(event.target.files[i]);
+      }
     }
-
-  }
-  submit(){
-
-    console.log(this.myForm.value);
-
-    this.http.post('http://localhost:8001/upload.php', this.myForm.value)
-
-      .subscribe(res => {
-
-        console.log(res);
-
-        alert('Uploaded Successfully.');
-
-      })
-
   }
 }
