bf8699ca44ff5a242816410dc3305e0ef528b537
souj <soujanya.venkatesh@gmail.com>
2020-08-25 19:20:52 +0530

    Web P

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,8 @@
-const gulp = require('gulp');
-const purify = require('gulp-purifycss');
+var gulp = require('gulp');
+var gzip = require('gulp-gzip');
 
-gulp.task('purifyCSS', () => {
-  return gulp.src('./dist/styles.*.css')
-    .pipe(
-      purify(
-        ['./src/app/**/*.ts', './src/app/**/*.html'],
-        {
-          info: true, // Outputs reduction information (like in the screenshot above)
-          minify: true, // Minifies the files after reduction
-          rejected: false, // Logs the CSS rules that were removed
-          whitelist: ['*transition*', '*dimmer*'] // Ignored css classes
-        }
-      ),
-    )
-    .pipe(gulp.dest('./dist/'));
+gulp.task('compress', function() {
+  return gulp.src(['./dist/**/*.*'])
+      .pipe(gzip())
+      .pipe(gulp.dest('./dist'));
 });
