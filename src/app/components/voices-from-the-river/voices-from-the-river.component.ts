169aa98f412c743af93bc7bcde0a0637c3815b86
unknown <joshi.paritosh07@gmail.com>
2020-12-23 20:18:13 +0530

    Blog Modal

--- src/app/components/voices-from-the-river/voices-from-the-river.component.ts
+++ src/app/components/voices-from-the-river/voices-from-the-river.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, FormArray } from '@angular/forms';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ModalDirective } from 'ng-uikit-pro-standard';
@@ -13,12 +13,13 @@ import { SpinnerService } from '../../services/spinner.service';
   styleUrls: ['./voices-from-the-river.component.scss']
 })
 export class VoicesFromTheRiverComponent implements OnInit {
-  blogs =[] ;
+  blogs = [];
+  public selectedBlog: any;
 
   constructor(private router: Router, private orolService: OrolService,
     private spinnerService: SpinnerService) {
-      this. getBlogs();
-     }
+    this.getBlogs();
+  }
 
   ngOnInit(): void {
 
@@ -30,22 +31,16 @@ export class VoicesFromTheRiverComponent implements OnInit {
   getBlogs() {
     // var a = this.orolService.getBlogs();
     console.log(this.blogs);
-    this.orolService.getBlogs().subscribe((data)=>{
-      if(data['count']){
-        for(var i=0; i<data['rows'].length;i++){
+    this.orolService.getBlogs().subscribe((data) => {
+      if (data['count']) {
+        for (var i = 0; i < data['rows'].length; i++) {
           this.blogs.push({
-            featuredTitle:data['rows'][i].featuredTitle,
-            featuredDescription:data['rows'][i].featuredDescription,
-            featuredPhoto:data['rows'][i].featuredPhoto,
-            createdAt:data['rows'][i].createdAt,
-            contributorName:data['rows'][i].contributorName,
-            // {
-//   featuredTitle :'Title of the news',
-//   featureDescription:'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione.',
-//   featuredPhoto:'https://mdbootstrap.com/img/Photos/Others/images/13.jpg',
-//   createdAt:'11/07/2018',
-//   blogerName:'Billy Forester'
-// },
+            featuredTitle: data['rows'][i].featuredTitle,
+            featuredDescription: data['rows'][i].featuredDescription,
+            featuredPhoto: data['rows'][i].featuredPhoto,
+            createdAt: data['rows'][i].createdAt,
+            contributorName: data['rows'][i].contributorName,
+            featuredAdditionalPhotos:data['rows'][i].featuredAdditionalPhotos,
           });
         }
       }
--- src/app/components/voices-from-the-river/voices-from-the-river.module.ts
+++ src/app/components/voices-from-the-river/voices-from-the-river.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule,FormsModule} from '@angular/forms';
 import { VoicesFromTheRiverRoutingModule } from './voices-from-the-river-routing.module';
 import { VoicesFromTheRiverComponent } from './voices-from-the-river.component';
-import {  ModalModule, WavesModule , InputsModule, ButtonsModule} from 'ng-uikit-pro-standard';
+import {  ModalModule, WavesModule , InputsModule, ButtonsModule,CarouselModule} from 'ng-uikit-pro-standard';
 import {AddBlogComponent} from '../add-blog/add-blog.component';
 @NgModule({
   declarations: [
@@ -16,6 +16,7 @@ import {AddBlogComponent} from '../add-blog/add-blog.component';
     ReactiveFormsModule,
     FormsModule,
     InputsModule,
+    CarouselModule,
     ModalModule,
     WavesModule,
     ButtonsModule
